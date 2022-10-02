5bc1f87bd2c60a3d86f7726b07b052d2d6c2430f
Anton <vsevsevse@ukr.net>
2022-10-02 03:07:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
